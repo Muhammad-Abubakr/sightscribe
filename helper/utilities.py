72a9f8f4936d505fd06e9e69b8b76259a150101c
Abubakr Siddique <abubakr.sidiqque@gmail.com>
2023-10-03 09:43:39 +0500

    initial commit

---
+++ helper/utilities.py
@@ -0,0 +1,5 @@
+def forFrame(frame_number, output_array, output_count):
+    print("FOR FRAME ", frame_number)
+    print("Output for each object : ", output_array)
+    print("Output count for unique objects : ", output_count)
+    print("------------END OF A FRAME --------------")
